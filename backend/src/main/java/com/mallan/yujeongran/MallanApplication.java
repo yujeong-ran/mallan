862dfa804dc1ca78fcd09b23952b4438a1ac6c73
DawnIsProblem <qweqwerty12321@gmail.com>
2025-05-09 20:32:43 +0900

    setting: initial setting

---
+++ backend/src/main/java/com/mallan/yujeongran/MallanApplication.java
@@ -0,0 +1,11 @@
+package com.mallan.yujeongran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallanApplication.class, args);
+    }
+}
