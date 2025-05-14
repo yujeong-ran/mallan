8af30e4d02e638e7497072fcc3f122e569f4e35a
유지수 <jsyu333@gmail.com>
2025-05-14 16:38:41 +0900

    style: Add GlobalStyle for consistent global styles

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+  background: linear-gradient(to bottom, #f5eaff, #fef8ff);
+`;
+
+function Home() {
+  return <Container></Container>;
+}
+
+export default Home;
