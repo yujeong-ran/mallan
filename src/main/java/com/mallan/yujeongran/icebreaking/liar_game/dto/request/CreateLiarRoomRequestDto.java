2cbdb4db30c4940ed2ae70d8dd41de307dc191d5
DawnIsProblem <qweqwerty12321@gmail.com>
2025-05-15 02:16:02 +0900

    setting: Add global exception handler

---
+++ src/main/java/com/mallan/yujeongran/icebreaking/liar_game/dto/request/CreateLiarRoomRequestDto.java
@@ -0,0 +1,11 @@
+package com.mallan.yujeongran.icebreaking.liar_game.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class CreateLiarRoomRequestDto {
+
+    private String hostId;
+    private String hostNickname;
+
+}
