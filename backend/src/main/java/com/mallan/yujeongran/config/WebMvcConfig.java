3d2a84462bedb6a5984e273c09d1b0c305592254
DawnIsProblem <qweqwerty12321@gmail.com>
2025-05-18 16:24:10 +0900

    feat: Add WebMcv Setting

---
+++ backend/src/main/java/com/mallan/yujeongran/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.mallan.yujeongran.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/common/profile_image/**")
+                .addResourceLocations("classpath:/static/common/profile_image/");
+    }
+
+}
