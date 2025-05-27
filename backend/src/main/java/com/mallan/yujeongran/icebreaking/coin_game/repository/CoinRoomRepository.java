b5861e44645f3a374e47417613953e7c9c6faf92
DawnIsProblem <qweqwerty12321@gmail.com>
2025-05-27 19:07:55 +0900

    feat: Add findByCreatedAtBefore method

--- backend/src/main/java/com/mallan/yujeongran/icebreaking/coin_game/repository/CoinRoomRepository.java
+++ backend/src/main/java/com/mallan/yujeongran/icebreaking/coin_game/repository/CoinRoomRepository.java
@@ -3,10 +3,13 @@ package com.mallan.yujeongran.icebreaking.coin_game.repository;
 import com.mallan.yujeongran.icebreaking.coin_game.entity.CoinRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface CoinRoomRepository extends JpaRepository<CoinRoom, Long> {
 
     Optional<CoinRoom> findByRoomCode(String roomCode);
+    List<CoinRoom> findByCreatedAtBefore(LocalDateTime time);
 
 }
