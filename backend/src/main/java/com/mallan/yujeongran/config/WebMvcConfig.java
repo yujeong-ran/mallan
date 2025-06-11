4d8055d139bc83c45272227c4adcd18a69727e4d
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-06-11 23:19:21 +0900

    [#47] CORS 설정 수정 (#48)

--- backend/src/main/java/com/mallan/yujeongran/config/WebMvcConfig.java
+++ backend/src/main/java/com/mallan/yujeongran/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.mallan.yujeongran.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/common/profile_image/**")
                 .addResourceLocations("classpath:/static/common/profile_image/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://port-0-mallan-mbae24le7f0ebadb.sel4.cloudtype.app"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
