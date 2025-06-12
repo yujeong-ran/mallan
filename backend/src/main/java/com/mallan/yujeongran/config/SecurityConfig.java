cb4c8f26c2a874fa4ac6777f65168fb7cedb5618
DawnIsProblem <qweqwerty12321@gmail.com>
2025-06-12 16:35:21 +0900

    hot-fix: Resolve CORS redundancy settings

--- backend/src/main/java/com/mallan/yujeongran/config/SecurityConfig.java
+++ backend/src/main/java/com/mallan/yujeongran/config/SecurityConfig.java
@@ -46,7 +46,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
 
                 )
-                .cors(Customizer.withDefaults())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
 
