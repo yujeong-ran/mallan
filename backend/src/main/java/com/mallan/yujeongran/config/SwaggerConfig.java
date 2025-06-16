bc832423f92817abfb8db7d1c684d2ac560fa5d0
DawnIsProblem <qweqwerty12321@gmail.com>
2025-06-17 01:14:59 +0900

    feat: Add authorization setting

--- backend/src/main/java/com/mallan/yujeongran/config/SwaggerConfig.java
+++ backend/src/main/java/com/mallan/yujeongran/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.mallan.yujeongran.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,14 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("Authorization",
+                                new SecurityScheme()
+                                        .type(Type.APIKEY)
+                                        .in(In.HEADER)
+                                        .name("Authorization")
+                        )
+                )
                 .info(apiInfo());
     }
 
@@ -22,27 +32,27 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .description(
                         """
-                        ## 🧊 아이스브레이킹 API 명세서
-                        ---
-                        ### 📍팀명
-                        🥚Mallan
-                        ### 🐣팀원
-                        🙋🏻‍♀️유지수(프론트엔드)
-                        \n\n
-                        🙋🏻‍♂️이정훈(백엔드)
-                        \n\n
-                        ### 🎮게임 항목
-                        - 밸런스게임
-                        - 라이어게임
-                        - 랜덤 퀘스천 뽑기게임
-                        - 공통점 찾기 게임
-                        - 동전 진실게임 
-                        ### 🔗 링크
-                        📌 [Mallan-Web] 등록 예정 
-                        📌 [GitHub](https://github.com/yujeong-ran/mallan) 
-                        📌 [Notion](https://www.notion.so/1eca7efc6b0380ee8f99f5d528ef8200)
-                        📌 [Jira](https://qweqwerty12321-1740141206278.atlassian.net/jira/software/projects/MAL/pages)
-                        """
+                                ## 🧊 아이스브레이킹 API 명세서
+                                ---
+                                ### 📍팀명
+                                🥚Mallan
+                                ### 🐣팀원
+                                🙋🏻‍♀️유지수(프론트엔드)
+                                \n\n
+                                🙋🏻‍♂️이정훈(백엔드)
+                                \n\n
+                                ### 🎮게임 항목
+                                - 밸런스게임
+                                - 라이어게임
+                                - 랜덤 퀘스천 뽑기게임
+                                - 공통점 찾기 게임
+                                - 동전 진실게임 
+                                ### 🔗 링크
+                                📌 [Mallan-Web] 등록 예정 
+                                📌 [GitHub](https://github.com/yujeong-ran/mallan) 
+                                📌 [Notion](https://www.notion.so/1eca7efc6b0380ee8f99f5d528ef8200)
+                                📌 [Jira](https://qweqwerty12321-1740141206278.atlassian.net/jira/software/projects/MAL/pages)
+                                """
                 );
     }
 
