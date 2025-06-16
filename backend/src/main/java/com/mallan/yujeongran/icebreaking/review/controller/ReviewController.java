7aab05c2e986be84657f161dd1e12c841d2c1c22
DawnIsProblem <qweqwerty12321@gmail.com>
2025-06-17 01:13:38 +0900

    feat: Add stats api path

--- backend/src/main/java/com/mallan/yujeongran/icebreaking/review/controller/ReviewController.java
+++ backend/src/main/java/com/mallan/yujeongran/icebreaking/review/controller/ReviewController.java
@@ -3,15 +3,15 @@ package com.mallan.yujeongran.icebreaking.review.controller;
 import com.mallan.yujeongran.common.model.CommonResponse;
 import com.mallan.yujeongran.icebreaking.review.dto.request.ReviewRequestDto;
 import com.mallan.yujeongran.icebreaking.review.dto.response.ReviewResponseDto;
+import com.mallan.yujeongran.icebreaking.review.dto.response.ReviewStatsResponseDto;
 import com.mallan.yujeongran.icebreaking.review.service.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reviews")
@@ -40,6 +40,18 @@ public class ReviewController {
     ) {
         ReviewResponseDto reviewResponseDto = reviewService.createReview(request);
         return ResponseEntity.ok(CommonResponse.success("리뷰 작성 성공!", reviewResponseDto));
+    }
+
+    @GetMapping("/recent")
+    @Operation(summary = "최근 2건의 리류 조회 API", description = "최근 리뷰 2건을 조회합니다.")
+    public ResponseEntity<CommonResponse<List<ReviewResponseDto>>> getRecentReviews() {
+        return ResponseEntity.ok(CommonResponse.success("최근 리뷰 2건 조회", reviewService.getRecentReviews()));
+    }
+
+    @GetMapping("/stats")
+    @Operation(summary = "리뷰 통계 조회 API", description = "총 후기 수, 평균 평점, 이번달 리뷰 수를 한번에 조회합니다.")
+    public ResponseEntity<CommonResponse<ReviewStatsResponseDto>> getReviewStats() {
+        return ResponseEntity.ok(CommonResponse.success("리뷰 통계 조회 성공", reviewService.getReviewStats()));
     }
 
 }
